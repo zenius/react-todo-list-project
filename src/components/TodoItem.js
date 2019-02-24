9af503236f79bc92c8f9d548132f0e21f2e27aae
Keshava Muraari <19950180+keshava19@users.noreply.github.com>
2019-02-24 23:40:55 +0530

    Text capitalize removed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ export default class TodoItem extends Component {
   render() {
     const { title, handleDelete, handleEdit } = this.props;
     return (
-      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+      <li className="list-group-item d-flex justify-content-between my-2">
         <h6>{title}</h6>
         <div className="todo-icon">
           <span className="mx-2 text-success" onClick={handleEdit}>
